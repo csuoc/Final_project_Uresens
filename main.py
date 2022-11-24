4c62d0fa8a8b967f2848a8688d2e22957050465a
Carles Sunyol <carles.sunyol@gmail.com>
2022-11-24 13:45:42 +0100

    Deploy streamlit

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.write("Hello world!")
+st.markdown("This is **bold** and this is _italics_")
+st.title('My first title')
+
---
+++ pages/about.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="About", page_icon="👋")
+
+st.write("Hello world!")
