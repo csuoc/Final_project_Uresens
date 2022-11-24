3ef1c8c44226a3427940364ddd15c19ee3c66a25
Carles Sunyol <carles.sunyol@gmail.com>
2022-11-24 16:31:09 +0100

    Streamlit SQL connection

--- pages/about.py
+++ pages/about.py
@@ -1,5 +1,8 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(page_title="About", page_icon="👋")
 
 st.write("Hello world!")
+
+#cover = Image.open("images/cover.jpg")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,27 @@
+# streamlit_app.py
+
+
 import streamlit as st
 import mysql.connector
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from patients;")
+
+# Print results.
+for row in rows:
+    st.write(f"Patient {row[0]} has a :{row[1]}:")
