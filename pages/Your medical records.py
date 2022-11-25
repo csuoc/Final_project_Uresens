1c6d8f2b87e77c8907029890ed74c0c4121e10e7
Carles Sunyol <carles.sunyol@gmail.com>
2022-11-25 17:13:02 +0100

    Prediction works!

--- pages/Your medical records.py
+++ pages/Your medical records.py
@@ -5,7 +5,9 @@ import streamlit as st
 import mysql.connector
 from streamlit_extras.app_logo import add_logo
 from functions.functions import add_text_sidebar
+from functions.functions import rename_columns
 import plotly.express as px
+import h2o
 
 # Head
 
@@ -20,7 +22,7 @@ def init_connection():
 
 conn = init_connection()
 
-# Perform query.
+# Query function
 
 def run_query(query):
     with conn.cursor() as cur:
@@ -33,15 +35,64 @@ st.title("Your medical records")
 
 # Retrieve from MySQL
 
-getSQL = f"""SELECT date, blood_pressure, albumin, sugar, blood_urea, creatinine FROM samples;
+getSQL = f"""SELECT date, blood_pressure, albumin, sugar, blood_urea, creatinine FROM samples
+            WHERE patientid="Carles";
         """
 result = pd.read_sql_query(getSQL, con=conn)
 
-# Save as dataframe
+# Convert as dataframe
 
 df = pd.DataFrame(result)
 
+# Rename columns
+
+new_name=["Date", "Blood Pressure mm/Hg diastolic", "Albumin (0-5)", "Sugar (0-5)", "Blood Urea mg/dL", "Serum Creatinine mg/dL", "Hypertension Yes/No"]
+for i, j in zip(df.columns, new_name):
+    rename_columns(df, i, j)
+
 # Plot
-y_axis_val = st.selectbox("Select Y axis", options=["blood_pressure", "albumin", "sugar", "blood_urea", "creatinine"])
-fig = px.line(df, x=df["date"], y=y_axis_val)
+y_axis_val = st.selectbox("Select Y axis", options=["Blood Pressure mm/Hg diastolic", "Albumin (0-5)", "Sugar (0-5)", "Blood Urea mg/dL", "Serum Creatinine mg/dL"])
+fig = px.scatter(df, x=df["Date"], y=y_axis_val)
 st.plotly_chart(fig)
+
+# Prediction
+
+h2o.init()
+
+# Retrieve from MySQL the required parameters
+
+pred = f"""SELECT blood_pressure, albumin, sugar, blood_urea, creatinine, hypertension FROM samples
+            WHERE patientid="Test1";
+        """
+result2 = pd.read_sql_query(pred, con=conn)
+df2 = pd.DataFrame(result2)
+
+# Rename columns
+
+new_name=["Blood Pressure mm/Hg diastolic", "Albumin (0-5)", "Sugar (0-5)", "Blood Urea mg/dL", "Serum Creatinine mg/dL", "Hypertension Yes/No"]
+for i, j in zip(df2.columns, new_name):
+    rename_columns(df2, i, j)
+
+#######################################
+
+st.title("Prediction")
+
+# Load file
+
+test = h2o.H2OFrame(df2)
+
+# Load the model
+
+my_model = h2o.load_model("./models/XRT_1_AutoML_1_20221124_232959")
+
+# Predict
+
+predictions = my_model.predict(test)
+
+if predictions["predict"] == 1:
+
+    st.write("You probably have Chronic Kidney Disease")
+
+elif predictions["predict"] == 0:
+
+    st.write("The model predicts you don't have Chronic Kidney Disease")
