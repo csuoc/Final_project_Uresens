88f0d728d197fe7f37d2c8796918fc35a95c4f39
Carles Sunyol <carles.sunyol@gmail.com>
2022-11-28 17:52:07 +0100

    Modifying home

--- Home.py
+++ Home.py
@@ -2,7 +2,8 @@ import streamlit as st
 from PIL import Image
 from streamlit_extras.app_logo import add_logo
 from functions.functions import add_text_sidebar
-
+from streamlit_card import card
+import matplotlib.image as mpimg
 
 st.set_page_config(
     page_title="UreSens App",
@@ -13,8 +14,24 @@ add_logo("https://i.ibb.co/TTF105V/logo2.png")
 add_text_sidebar()
 
 
-cover = Image.open("images/logo2.png")
-st.image(cover)
-st.write("Hello world!")
-st.markdown("This is **bold** and this is _italics_")
-st.title('My first title')
+card(
+    title="About UreSens",
+    text="Discover who's behind the app",
+    image="https://i.ibb.co/JqQjkmK/Carles.jpg",
+    url="http://localhost:8501/About#hello",
+)
+
+card(
+    title="What is CKD?",
+    text="Symptoms, causes, treatment",
+    image="https://i.ibb.co/m4Dr1t0/CKD1.jpg",
+    url="http://localhost:8501/About#hello",
+)
+
+card(
+    title="Submit your results",
+    text="Take an urine test and upload here your results",
+    image="https://i.ibb.co/gV5K0kS/urinetest.jpg",
+    url="http://localhost:8501/About#hello",
+)
+
